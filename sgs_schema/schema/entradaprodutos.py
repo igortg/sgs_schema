0a4a7ae97497dcd48c1366da9f72d92f3fecb661
Igor Ghisi <igor.ghisi@gmail.com>
2017-06-18 10:30:39 -0300

    Implementa esquema para associação ENTRADA e PRODUTOS
    Implementa associação de itens da tabela ENTRADA com itens da tabela
    PRODUTO através da tabela ENTRADA_ITENS. Fez-se uso do modelo de
    association proxy do SQLAlchemy para tal.

--- sgs_schema/schema/entradaprodutos.py
+++ sgs_schema/schema/entradaprodutos.py
@@ -1,6 +1,11 @@
-from sqlalchemy import Column, Integer, Sequence, Date, SmallInteger, String, TIMESTAMP
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, Sequence, Date, SmallInteger, String, TIMESTAMP, ForeignKey, Float
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import relationship, backref
 
 from sgs_schema.declarative_base import Base
+from sgs_schema.schema.produto import Produto
 
 
 class EntradaProduto(Base):
@@ -25,3 +30,35 @@ class EntradaProduto(Base):
     classname = Column(String(100))
     id_mov = Column(Integer)
 
+    produtos = association_proxy("entrada_itens", "produto")
+
+    def __init__(self, *args, **kw):
+        super(EntradaProduto, self).__init__(*args, **kw)
+        self.createat = datetime.now()
+
+
+class EntradaProdutoItens(Base):
+
+    __tablename__ = "ENTRADA_ITENS"
+
+    id = Column(Integer, Sequence("ID_MANAGER"), primary_key=True)
+    id_entrada = Column(Integer, ForeignKey(EntradaProduto.id))
+    entrada_produto = relationship(EntradaProduto, backref=backref("entrada_itens", cascade="all, delete-orphan"))
+    id_produto = Column(Integer, ForeignKey(Produto.id))
+    produto = relationship(Produto)
+    quantidade = Column(Float("DOUBLE PRECISION"))
+    valor = Column(Float("DOUBLE PRECISION"))
+    fator_unidade = Column(Float("DOUBLE PRECISION"), default=1)
+    desconto = Column(Float("DOUBLE PRECISION"), default=0)
+    id_valorat1 = Column(Integer, nullable=False)
+    id_valorat2 = Column(Integer, nullable=False)
+    createat = Column(TIMESTAMP)
+    updateat = Column(TIMESTAMP)
+    classname = Column(String(100))
+
+
+    def __init__(self, produto=None):
+        self.produto = produto
+        self.id_valorat1 = -1
+        self.id_valorat2 = -1
+        self.createat = datetime.now()
--- sgs_schema/schema/tests/test_entrada.py
+++ sgs_schema/schema/tests/test_entrada.py
@@ -1,9 +1,10 @@
 from datetime import date, datetime
 
-from sgs_schema.schema.entradaprodutos import EntradaProduto
+from sgs_schema.schema.entradaprodutos import EntradaProduto, EntradaProdutoItens
+from sgs_schema.schema.produto import Produto, CategoriaProduto
 
 
-def test_entrada_produtos(db_session):
+def test_entrada_produto(db_session):
     entrada = EntradaProduto()
     entrada.id_empresa = 1
     entrada.data = date(2017, 6, 17)
@@ -16,7 +17,17 @@ def test_entrada_produtos(db_session):
     db_session.add(entrada)
     db_session.commit()
 
-    rentrada = db_session.query(EntradaProduto).filter(entrada.id == 1).first()
+    produtos = cria_produtos(db_session)
+
+    entrada.produtos.append(produtos[0])
+    entrada.produtos.append(produtos[1])
+    # Testa que o association_proxy está funcionando
+    assert entrada.produtos == [produtos[0], produtos[1]]
+
+    db_session.add(entrada)
+    db_session.commit()
+
+    rentrada = db_session.query(EntradaProduto).filter(entrada.numero == 1).first()
     assert rentrada.id_empresa == 1
     assert rentrada.data == date(2017, 6, 17)
     assert rentrada.tipo == 2
@@ -24,3 +35,37 @@ def test_entrada_produtos(db_session):
     assert rentrada.numero == 1
     assert rentrada.historico == entrada.historico
     assert rentrada.createat == datetime(2017, 6, 17, 11, 00)
+
+    rproduto = rentrada.produtos[0]
+    rproduto.codbarra = "00213"
+    rproduto.codigo = "AN00213"
+
+
+def test_entrada_produto_itens(db_session):
+    produtos = cria_produtos(db_session)
+
+    entrada_itens = EntradaProdutoItens()
+    entrada_itens.produto = produtos[0]
+    db_session.add(entrada_itens)
+    db_session.commit()
+
+    r_entrada_itens = db_session.query(EntradaProdutoItens).filter(EntradaProdutoItens.id == entrada_itens.id).first()
+    assert r_entrada_itens.produto == entrada_itens.produto
+
+
+def cria_produtos(db_session):
+    categoria = CategoriaProduto()
+    categoria.description = "Anel"
+    db_session.add(categoria)
+    db_session.commit()
+
+    dados = [
+        ("00213", "AN00213", "Anel Zirconia"),
+        ("00214", "BR00214", "Brinco Pedra"),
+        ("00215", "PS00215", "Pulseira Infantil"),
+    ]
+
+    produtos = [Produto(codbarra=item[0], codigo=item[1], descricao=item[2], categoria=categoria) for item in dados]
+    db_session.add_all(produtos)
+    db_session.commit()
+    return produtos
